a5c4d7860cd01b6e318ed30b1adba2fe7c1cc509
Maureen E <maureenekeugo1@gmail.com>
2024-07-03 09:59:42 -0400

    Added local storage for dark mode

--- script.js
+++ script.js
@@ -2,13 +2,37 @@ const settingsMenu = document.querySelector('.settings-menu')
 const darkBtn = document.getElementById('dark-btn')
 
 const  settingsMenuToggle =()=>{
-    settingsMenu.classList.toggle('settings-menu-height')
+    settingsMenu.classList.toggle('settings-menu-height');
 }
 
 
 darkBtn.onclick = ()=>{
-    darkBtn.classList.toggle('dark-btn-on')
-    document.body.classList.toggle('dark-theme')
+    darkBtn.classList.toggle('dark-btn-on');
+    document.body.classList.toggle('dark-theme');
+
+    if(localStorage.getItem('theme')=='light'){
+    localStorage.setItem('theme', 'dark');
+        
+    }
+    else{
+    localStorage.setItem('theme', 'light');
+
+    }
 
 } 
+
+// Local Storage
+
+if(localStorage.getItem('theme')=='light'){
+    darkBtn.classList.remove('dark-btn-on');
+    document.body.classList.remove('dark-theme')
+}
+else if(localStorage.getItem('theme')=='dark'){
+    darkBtn.classList.add('dark-btn-on');
+    document.body.classList.add('dark-theme')
+}
+else{
+    localStorage.setItem('theme', 'light');
+}
+
 
